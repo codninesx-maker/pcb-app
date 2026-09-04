66de3c56c1db5c0d8820852d69897d3e0b971e9b
Md Sohail Rana <codninesxl@gmail.com>
2026-09-04 10:12:15 +0600

    Force add missing web helper files

--- lib/web_helper_web.dart
+++ lib/web_helper_web.dart
@@ -1,9 +1,3 @@
 import 'dart:html' as html;
-
-String getWebWindowHash() {
-  return html.window.location.hash;
-}
-
-void clearWebWindowHash() {
-  html.window.history.replaceState(null, '', html.window.location.pathname);
-}
+String getWebWindowHash() => html.window.location.hash;
+void clearWebWindowHash() { html.window.history.replaceState(null, '', html.window.location.pathname); }
